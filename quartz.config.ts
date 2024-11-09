840f01a72fcb213aa2c70c2301da20deb19f6f29
theodotdot <59372255+theodotdot@users.noreply.github.com>
2024-11-09 16:31:03 +0100

    Quartz sync: Nov 9, 2024, 4:31 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,9 +23,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Fira Sans",
+        body: "Fira Sans",
+        code: "Fira Code",
       },
       colors: {
         lightMode: {
