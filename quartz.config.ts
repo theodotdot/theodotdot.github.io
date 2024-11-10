d991f9d5599a370687c04b377bfd82fe13f0e0c3
theodotdot <59372255+theodotdot@users.noreply.github.com>
2024-11-10 21:01:52 +0100

    Quartz sync: Nov 10, 2024, 9:01 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://theodotdot.github.io/miettes/",
+    baseUrl: "https://theodotdot.github.io/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
